717d82b4ace739ebf290f4ca0b6cec23dbccdff4
Egio, Alfonso <alfonso.egio@i2cat.net>
2019-08-23 12:30:26 +0200

    Fix stakeDue, pending balance check accounting for fee

--- contracts/InternetAccessETH.sol
+++ contracts/InternetAccessETH.sol
@@ -14,8 +14,9 @@ contract InternetAccessETH is Ownable {
   uint256 private onFlyBalance;
   uint256 private stakeDue;
 
-  event ConnectionRequest(address indexed _from, uint256 _value, bool _stake, uint256 _balance, uint256 _onFlyNumber);
-  event EarningsCollection(uint256 _currentTimestamp, uint256 _connectionTimestamp);
+  event ConnectionRequest(address indexed _from, uint256 _value, bool _stake, uint256 _balance, uint256 _onFlyNumber, uint256 _stakeDue);
+  event EarningsCollection(uint256 _currentTimestamp, uint256 _connectionTimestamp, uint256 _amount, uint256 _balance);
+  event TotalEarningsCollection(uint256 _amount, uint256 _balance, uint256 _stakeDue);
 
   struct Connection {
     uint256 timestamp;
@@ -23,9 +24,10 @@ contract InternetAccessETH is Ownable {
     bool withStake;
   }
 
-  mapping (address => Connection) public connections;
+  mapping (uint256 => Connection) public connections;
 
   struct OnFlyConnection {
+    uint256 key;
     bool allocated;
     address user;
   }
@@ -49,16 +51,19 @@ contract InternetAccessETH is Ownable {
     require(msg.value <= maxPayment, "Value over maximum");
     bool withStake;
     withStake = (address(this).balance.sub(msg.value).sub(onFlyBalance).sub(stakeDue) > 0);
-    connections[msg.sender].timestamp = now;
-    connections[msg.sender].amount = msg.value;
-    connections[msg.sender].withStake = withStake;
+    uint256 timestamp = now;
+    uint256 connectionKey = uint256(keccak256(abi.encodePacked(msg.sender, msg.value, timestamp, withStake)));
+    connections[connectionKey].timestamp = timestamp;
+    connections[connectionKey].amount = msg.value;
+    connections[connectionKey].withStake = withStake;
     onFlyBalance.add(msg.value);
     if (withStake) {
-      stakeDue.add(msg.value);
+      stakeDue = stakeDue.add(msg.value);
     }
+    onFlyConnections[uint256(onFlyNumber)].key = connectionKey;
     onFlyConnections[uint256(onFlyNumber)].user = msg.sender;
     onFlyConnections[uint256(onFlyNumber)].allocated = true;
-    emit ConnectionRequest(msg.sender, msg.value, withStake, address(this).balance, uint256(onFlyNumber));
+    emit ConnectionRequest(msg.sender, msg.value, withStake, address(this).balance, uint256(onFlyNumber), stakeDue);
     return;
   }
 
@@ -78,16 +83,24 @@ contract InternetAccessETH is Ownable {
      @dev Owner collects earnings.
      Some balance is left, in order to afford stake on future connections.
   */
-  function collectEarnings(uint _balanceLeft) public {
+  function collectEarnings() public {
     require(isOwner(), "Only contract owner can collect earnings");
-    uint availableEarnings; /** Available earnings */
-    uint lastTimestampAllowed = now.sub(3600 * 24); /** 24h aprox. */
-    uint i;
-    availableEarnings = address(this).balance.sub(_balanceLeft);
-    while (i < 10 && availableEarnings > 0) {
+    uint256 lastTimestampAllowed = now.sub(3600 * 24); /** 24h aprox. */
+    uint256 i;
+    uint256 availableEarnings = 0;
+    while (i < 10) {
       if (onFlyConnections[i].allocated) {
-        if (connections[onFlyConnections[i].user].timestamp < lastTimestampAllowed) {
-          emit EarningsCollection(now, connections[onFlyConnections[i].user].timestamp);
+        if (connections[onFlyConnections[i].key].timestamp < lastTimestampAllowed) {
+          emit EarningsCollection(now,
+                                  connections[onFlyConnections[i].key].timestamp,
+                                  connections[onFlyConnections[i].key].amount,
+                                  address(this).balance);
+          onFlyConnections[i].allocated = false;
+          availableEarnings = availableEarnings.add(connections[onFlyConnections[i].key].amount);
+          connections[onFlyConnections[i].key].amount = 0;
+          if(connections[onFlyConnections[i].key].withStake) {
+            stakeDue = stakeDue.sub(connections[onFlyConnections[i].key].amount);
+          }
         }
         /*       availableEarnings.sub(connections[onFlyConnections[i].user].amount); */
         /*       if (connections[onFlyConnections[i].user].withStake) */
@@ -96,13 +109,14 @@ contract InternetAccessETH is Ownable {
         /*       onFlyBalance.sub(connections[onFlyConnections[i].user].amount); */
         /*       if (connections[onFlyConnections[i].user].withStake) */
         /*         stakeDue.sub(connections[onFlyConnections[i].user].amount); */
-        onFlyConnections[i].allocated = false;
         /*     } */
       }
       i++;
     }
-    /* require(availableEarnings > 0, "There isn't balance enough"); */
-    /* msg.sender.transfer(availableEarnings); */
+    assert(availableEarnings <= address(this).balance);
+    emit TotalEarningsCollection(availableEarnings, address(this).balance, stakeDue);
+    stakeDue = stakeDue.sub(availableEarnings);
+    msg.sender.transfer(availableEarnings);
   }
 
   /**
@@ -110,17 +124,17 @@ contract InternetAccessETH is Ownable {
      Hat user's last connection stake, then both user and owner will lose their funds.
      Hat user's last connection no stake, then user will get a refund.
   */
-  function penalize () public {
-    uint256 i = 0;
-    while (i < 10 && onFlyConnections[i].user != msg.sender) i++;
-    require(i < 10 && onFlyConnections[i].allocated);
-    onFlyConnections[i].allocated = false;
-    onFlyBalance.sub(connections[msg.sender].amount);
-    if (connections[msg.sender].withStake) {
-      stakeDue.sub(connections[msg.sender].amount);
-      address(0).transfer(connections[msg.sender].amount.mul(2));
-    } else {
-      msg.sender.transfer(connections[msg.sender].amount);
-    }
-  }
+  /* function penalize () public { */
+  /*   uint256 i = 0; */
+  /*   while (i < 10 && onFlyConnections[i].user != msg.sender) i++; */
+  /*   require(i < 10 && onFlyConnections[i].allocated); */
+  /*   onFlyConnections[i].allocated = false; */
+  /*   onFlyBalance.sub(connections[msg.sender].amount); */
+  /*   if (connections[msg.sender].withStake) { */
+  /*     stakeDue.sub(connections[msg.sender].amount); */
+  /*     address(0).transfer(connections[msg.sender].amount.mul(2)); */
+  /*   } else { */
+  /*     msg.sender.transfer(connections[msg.sender].amount); */
+  /*   } */
+  /* } */
 }
--- tests/mainETH.test.js
+++ tests/mainETH.test.js
@@ -57,23 +57,40 @@ describe('ETH smart contract tests', function () {
     currentBalance.should.equal('3000000000000000')
   })
 
+  it('Should allow to require a second connection with ETH and stake', async () => {
+    let abi = JSON.parse(FS.readFileSync('./contracts/abiETH.json', 'utf-8'))
+    let accounts = await WEB3.eth.getAccounts()
+    let contract = new WEB3.eth.Contract(abi, process.env.CONTRACT_ETH_ADDRESS)
+    let previousBalance = await WEB3.eth.getBalance(process.env.CONTRACT_ETH_ADDRESS)
+    previousBalance.should.equal('3000000000000000')
+    let connection = await contract.methods.reqConnectionWithETH().send(
+      { from: accounts[2], value: '2000000000000000', gas: '1000000' })
+    connection.should.have.property('events')
+    connection.events.should.have.property('ConnectionRequest')
+    connection.events.ConnectionRequest.returnValues._from.should.equal(accounts[2])
+    connection.events.ConnectionRequest.returnValues._stake.should.equal(true)
+    connection.events.ConnectionRequest.returnValues._onFlyNumber.should.equal('1')
+    let currentBalance = await WEB3.eth.getBalance(process.env.CONTRACT_ETH_ADDRESS)
+    currentBalance.should.equal('5000000000000000')
+  })
+
   it('Should allow two different users to require connection with ETH', async () => {
     let abi = JSON.parse(FS.readFileSync('./contracts/abiETH.json', 'utf-8'))
     let accounts = await WEB3.eth.getAccounts()
     let contract = new WEB3.eth.Contract(abi, process.env.CONTRACT_ETH_ADDRESS)
     let connectionA = await contract.methods.reqConnectionWithETH().send(
-      { from: accounts[1], value: '3000000000000000', gas: '1000000' })
+      { from: accounts[3], value: '3000000000000000', gas: '1000000' })
     connectionA.should.have.property('events')
     connectionA.events.should.have.property('ConnectionRequest')
-    connectionA.events.ConnectionRequest.returnValues._from.should.equal(accounts[1])
-    connectionA.events.ConnectionRequest.returnValues._onFlyNumber.should.equal('1')
+    connectionA.events.ConnectionRequest.returnValues._from.should.equal(accounts[3])
+    connectionA.events.ConnectionRequest.returnValues._onFlyNumber.should.equal('2')
     connectionA.events.ConnectionRequest.returnValues._stake.should.equal(true)
     let connectionB = await contract.methods.reqConnectionWithETH().send(
-      { from: accounts[2], value: '3000000000000000', gas: '1000000' })
+      { from: accounts[4], value: '3000000000000000', gas: '1000000' })
     connectionB.should.have.property('events')
     connectionB.events.should.have.property('ConnectionRequest')
-    connectionB.events.ConnectionRequest.returnValues._from.should.equal(accounts[2])
-    connectionB.events.ConnectionRequest.returnValues._onFlyNumber.should.equal('2')
+    connectionB.events.ConnectionRequest.returnValues._from.should.equal(accounts[4])
+    connectionB.events.ConnectionRequest.returnValues._onFlyNumber.should.equal('3')
     connectionB.events.ConnectionRequest.returnValues._stake.should.equal(true)
   })
 
@@ -81,14 +98,14 @@ describe('ETH smart contract tests', function () {
     let abi = JSON.parse(FS.readFileSync('./contracts/abiETH.json', 'utf-8'))
     let accounts = await WEB3.eth.getAccounts()
     let contract = new WEB3.eth.Contract(abi, process.env.CONTRACT_ETH_ADDRESS)
-    for (let i = 0; i < 10; i++) {
+    for (let i = 4; i < 20; i++) {
       try {
         let connection = await contract.methods.reqConnectionWithETH().send(
           { from: accounts[i], value: '3000000000000000', gas: '1000000' })
-        connection.events.ConnectionRequest.returnValues._onFlyNumber.should.equal((3 + i).toString())
+        connection.events.ConnectionRequest.returnValues._onFlyNumber.should.equal(i.toString())
       } catch (error) {
-        // Since previous test allocated 3 connections (0, 1, 2) we have 7 left
-        i.should.be.above(6)
+        // Since previous test allocated 4 connections (0, 1, 2, 3) we have 6 left
+        i.should.be.above(9)
       }
     }
   })
@@ -110,8 +127,39 @@ describe('ETH smart contract tests', function () {
     let accounts = await WEB3.eth.getAccounts()
     let contract = new WEB3.eth.Contract(abi, process.env.CONTRACT_ETH_ADDRESS)
     await TIME_TRAVEL(12 * 3600)
-    let collectResponse = await contract.methods.collectEarnings('0').send(
+    let previousBalance = await WEB3.eth.getBalance(accounts[0])
+    let gasPrice = await WEB3.eth.getGasPrice()
+    let collectResponse = await contract.methods.collectEarnings().send(
       { from: accounts[0], gas: '1000000' })
-    collectResponse.events.should.eql({})
+    console.log(gasPrice)
+    console.log(JSON.stringify(collectResponse, null, 4))
+    let currentBalance = await WEB3.eth.getBalance(accounts[0])
+    currentBalance.should.equal(previousBalance)
+    collectResponse.events.TotalEarningsCollection.returnValues._amount.should.equal('0')
+    collectResponse.events.TotalEarningsCollection.returnValues._balance.should.equal('29000000000000000')
+  })
+
+  xit('Should allow the owner to collect earnings after 24 hours', async () => {
+    await TIME_TRAVEL(36 * 3600)
+    let abi = JSON.parse(FS.readFileSync('./contracts/abiETH.json', 'utf-8'))
+    let accounts = await WEB3.eth.getAccounts()
+    let contract = new WEB3.eth.Contract(abi, process.env.CONTRACT_ETH_ADDRESS)
+    let collectResponse = await contract.methods.collectEarnings().send(
+      { from: accounts[0], gas: '1000000' })
+    let counter = 0
+    collectResponse.events.EarningsCollection.forEach(
+      (x) => {
+        let currentTimestamp = parseInt(x.returnValues._currentTimestamp)
+        let connectionTimestamp = parseInt(x.returnValues._connectionTimestamp)
+        let delta = currentTimestamp - connectionTimestamp
+        delta.should.be.above(24 * 3600)
+        counter++
+        console.log(x.returnValues._amount)
+        console.log(x.returnValues._balance)
+      })
+    console.log(JSON.stringify(collectResponse, null, 4))
+    collectResponse.events.TotalEarningsCollection.returnValues._amount.should.equal('30000000000000000')
+    collectResponse.events.TotalEarningsCollection.returnValues._balance.should.equal('30000000000000000')
+    counter.should.equal(10)
   })
 })
